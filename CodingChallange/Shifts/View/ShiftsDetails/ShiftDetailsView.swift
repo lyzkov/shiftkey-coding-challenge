21871ba8fb775ef48a949b8188f54cf4bd5216f8
lyzkov <lyzkov@gmail.com>
2021-06-19 14:13:21 +0200

    #5 Shares store injected from dependency container

---
+++ CodingChallange/Shifts/View/ShiftsDetails/ShiftDetailsView.swift
@@ -0,0 +1,40 @@
+//
+//  ShiftDetailsView.swift
+//  CodingChallange
+//
+//  Created by lyzkov on 10/06/2021.
+//
+
+import SwiftUI
+
+struct ShiftDetailsView: View {
+    let details: ShiftDetails
+    
+    var body: some View {
+        NavigationView {
+            Group {
+                VStack(alignment: .leading) {
+                    Text("Shift ID: \(details.id)")
+                    Text("Facility: \(details.facility)")
+                    Text("Skill: \(details.skill)")
+                    Text("Specialty: \(details.specialty)")
+                    Text("Kind: \(details.kind)")
+                    HStack {
+                        Text(Date(), style: .date)
+                        Spacer()
+                        Text(Date(), style: .date)
+                    }
+                    .padding(.horizontal, 60.0)
+                    Spacer()
+                }
+            }
+            .navigationTitle(/*@START_MENU_TOKEN@*/"Shift details"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ShiftDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShiftDetailsView(details: .fake())
+    }
+}
