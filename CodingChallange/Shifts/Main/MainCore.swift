ecb83223fecdad6170af0e0383de06140a0bbee1
Piotr Łyczba <lyzkov@gmail.com>
2021-07-08 00:34:23 +0200

    #5 Module abstraction

--- CodingChallange/Shifts/Main/MainCore.swift
+++ CodingChallange/Shifts/Main/MainCore.swift
@@ -6,32 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
 
 import Common
 
 import ComposableArchitecture
 
-public class Main {
-    
-    public struct State: MainState, Equatable {
-        public var list: List.State = .idle
-        public var details: Details.State = .idle
-        
-        public init() {
-        }
-    }
-    
-    public enum Action: MainAction {
-        case list(List.Action)
-        case details(Details.Action)
-    }
-    
-    public typealias Reducer = ComposableArchitecture.Reducer<State, Action, Environment>
+public struct Main: Module {
 
     @Register
-    public var reducer = Reducer.combine(
+    var reducer = Reducer.combine(
         [
-            effectsReducer,
+            Reducer { state, action, environment in
+                switch action {
+                case .list(.load):
+                    state.list = .loading
+                    return .init(value: .list(.show(shifts: (3...60).map { _ in .fake() })))
+                case .details(.load(let id)):
+                    state.details = .loading
+                    if let selected = state.list.items?.first(by: id) {
+                        return .init(value: .details(.show(shift: selected)))
+                    }
+                default:
+                    break
+                }
+                
+                return .none
+            },
             List.reducer.pullback(
                 state: \State.list,
                 action: /Action.list,
@@ -44,31 +45,29 @@ public class Main {
             )
         ]
     )
-
-    private static let effectsReducer = Reducer { state, action, environment in
-        switch action {
-        case .list(.load):
-            state.list = .loading
-            return .init(value: .list(.show(shifts: (3...60).map { _ in .fake() })))
-        case .details(.load(let id)):
-            state.details = .loading
-            if let selected = state.list.items?.first(by: id) {
-                return .init(value: .details(.show(shift: selected)))
-            }
-        default:
-            break
-        }
-        
-        return .none
-    }
     
-    public struct Environment: MainEnvironemnt {
-        public init() {
-        }
-    }
+    public var view: some View = List.View()
     
     public init() {
     }
     
 }
 
+extension Main {
+    
+    struct State: MainState, Equatable {
+        var list: List.State = .idle
+        var details: Details.State = .idle
+    }
+    
+    enum Action: MainAction {
+        case list(List.Action)
+        case details(Details.Action)
+    }
+    
+    struct Environment: MainEnvironemnt {
+    }
+    
+    typealias Reducer = ComposableArchitecture.Reducer<State, Action, Environment>
+    
+}
