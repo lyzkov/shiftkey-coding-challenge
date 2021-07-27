5a4a01f63c393c9859e96e26108cea6ea1dbc9ca
Piotr Łyczba <lyzkov@gmail.com>
2021-07-27 20:28:42 +0200

    #8 Paging Shifts List by start date and loading next page after scrolling to the last item on view

--- CodingChallenge/Shifts/List/ListView.swift
+++ CodingChallenge/Shifts/List/ListView.swift
@@ -15,31 +15,49 @@ extension Shifts.List {
     
     public struct View: ComposableView {
         
-        public typealias State = Load<IdentifiedArrayOf<Item>, Main.Error>?
+        public typealias State = IdentifiedArrayOf<PageByDate<Item, Main.Error>>
         
         @Resolve(state: \Main.State.list, action: Main.Action.list)
         var store: Store<State, Action>
         
         @SwiftUI.State var selected: Item? = nil
         
+        var forEachStore: Store<State, (Date, Action)> {
+            store.scope { state in
+                state
+            } action: { action in
+                Action.show(from: action.0)
+            }
+        }
+        
         public var body: some SwiftUI.View {
-            LoadStore(store, load: .show) { store in
-                NavigationView {
-                    SwiftUI.List(store.state) { item in
-                        List.ItemView(item: item)
-                            .onTapGesture {
-                                selected = item
+            NavigationView {
+                SwiftUI.List { // TODO: Paged List with store
+                    ForEachStore(forEachStore) { store in
+                        let date = ViewStore(store).currentDate
+                        LoadStore(store.scope(state: \.items), load: .show(from: date)) { items in
+                            if !items.isEmpty {
+                                Section {
+                                    ForEach(items.state) { item in
+                                        List.ItemView(item: item)
+                                            .onTapGesture {
+                                                selected = item
+                                            }
+                                    }
+                                }
                             }
+                        } progress: { store in
+                            ProgressView(value: store.state)
+                        } recovery: { _ in }
                     }
-                    .sheet(item: $selected) { item in
-                        Details.View(id: item.id)
-                    }
-                    .navigationTitle("Shifts")
                 }
-            } progress: { store in
-                ProgressView(value: store.state)
-            } recovery: { _ in
-                Text("Ooops")
+                .sheet(item: $selected) { item in
+                    Details.View(id: item.id)
+                }
+                .navigationTitle("Shifts")
+                .onAppear {
+                    ViewStore(store).send(.show())
+                }
             }
         }
     }
@@ -63,18 +81,13 @@ extension Shifts.List.View: FakeView {
 
 struct ListView_Previews: PreviewProvider {
     
+    static let fakes = (3...60).map { _ in Shifts.List.Item(from: .fake()) }
+    
     static var previews: some SwiftUI.View {
         Shifts.List.View.fake(
-            with: .completed(
-                .success(
-                    IdentifiedArrayOf(
-                        uniqueElements: (3...60)
-                            .map { _ in
-                                Shifts.List.Item(from: .fake())
-                            }
-                    )
-                )
-            )
+            with: [
+                Page(index: Date(), items: .completed(.success(.init(uniqueElements: fakes))))
+            ]
         )
     }
     
